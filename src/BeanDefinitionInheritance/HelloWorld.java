e1e48b5e627f83131fcf29b229dc62f399cceba8
CodechefVaibhav <vaibhav.kashyap@hotmail.com>
2013-10-14 00:11:24 +0530

    Inheritance in spring

---
+++ src/BeanDefinitionInheritance/HelloWorld.java
@@ -0,0 +1,32 @@
+package BeanDefinitionInheritance;
+
+
+	public class HelloWorld {
+		   private String message1;
+		   private String message2;
+		   private String message3;
+
+		   public String getMessage3() {
+			return message3;
+		   }
+
+		   public void setMessage3(String message3) {
+				this.message3 = message3;
+		   }
+	
+		   public void setMessage1(String message){
+			    this.message1  = message;
+		   }
+
+		   public void setMessage2(String message){
+		      this.message2  = message;
+		   }
+
+		   public void getMessage1(){
+		      System.out.println("World Message1 : " + message1);
+		   }
+
+		   public void getMessage2(){
+		      System.out.println("World Message2 : " + message2);
+		   }
+		}
